c7590396405c27e9d338f8bc97e44b930b957449
nicosis <nicosis@gmail.com>
2023-04-15 10:36:12 +0000

    work done

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/ToDoList.css";
+import { FiDelete } from "react-icons/fi";
 
 const ToDoList = () => {
   const [tasks, setTasks] = useState([]);
-  const [currentTask, setCurrentTask] = useState('');
-  // console.log(currentTask);
+  const [currentTask, setCurrentTask] = useState("");
+
+  //actualizo localmente el array con la lista de tareas
+  const getAllTaks = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/nicosis")
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllTaks();
+  }, []);
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && currentTask.trim()) {
@@ -12,26 +24,40 @@ const ToDoList = () => {
         label: currentTask,
         done: false,
       };
-
-      setTasks([aux, ...tasks])
+      setTasks([aux, ...tasks]);
       setCurrentTask("");
-      console.log(currentTask);
+      // actualizar el array con la tarea agregada a la lista, en el servidor
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/nicosis", {
+        method: "PUT",
+        body: JSON.stringify(tasks),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => {
+          console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+          console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+          console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+          return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+        })
+        .then((data) => {
+          //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+          console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        })
+        .catch((error) => console.log(error));
     }
   };
 
-  const getAllTaks = () => {
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/nicosis")
-    .then((response) => response.json())
-    .then((data) => setTasks(data))
-    .catch((error) => console.log(error))
-  }
+  const handleDelete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1); // elimina la tarea del array en el índice especificado
+    console.log(index);
+    setTasks(updatedTasks) // actualizo localmente el array sin la tarea eliminada
 
-  useEffect(() => {getAllTaks()}, []);
-
-  // useEffect(() => {
+    // actualizar el array sin la tarea eliminada en el servidor
     fetch("https://assets.breatheco.de/apis/fake/todos/user/nicosis", {
       method: "PUT",
-      body: JSON.stringify(tasks),
+      body: JSON.stringify(updatedTasks),
       headers: {
         "Content-Type": "application/json",
       },
@@ -43,13 +69,11 @@ const ToDoList = () => {
         return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
       })
       .then((data) => {
-        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+        // setTasks(updatedTasks); // Actualizar el estado local de la matriz de tareas
       })
-      .catch((error) => {
-        console.log(error);
-      });
-  // }, [tasks]);
+      .catch((error) => console.log(error));
+  };
 
   return (
     <div className="container my-5 w-50">
@@ -71,6 +95,12 @@ const ToDoList = () => {
             key={index}
           >
             {item.label}
+            <span>
+              <FiDelete
+                className={`trash-icon`}
+                onClick={() => handleDelete(index)}
+              />
+            </span>
           </li>
         ))}
       </ul>
